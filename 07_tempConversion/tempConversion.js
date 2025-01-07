d4fa72d2eb3b8267dfd87140d0043a853dd24e75
Ash <hrksingh17@gmail.com>
2025-01-07 10:49:45 +0530

    temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  return roundToOne(((temp - 32) * 5) / 9);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  return roundToOne((temp * 9) / 5 + 32);
 };
+
+function roundToOne(tempC) {
+  return Math.round(tempC * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
