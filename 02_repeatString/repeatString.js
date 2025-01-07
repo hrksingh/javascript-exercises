e5c6bd699b09c5f8aff14bfc3f12c90746ae50e6
Ash <hrksingh17@gmail.com>
2025-01-07 07:09:09 +0530

    2 exercises done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function (word, times) {
+  if (times < 0) return "ERROR";
+  let repeatedWord = "";
+  for (let i = 0; i < times; i++) {
+    repeatedWord += word;
+  }
+  return repeatedWord;
 };
-
+console.log(repeatString("hey", 3));
 // Do not edit below this line
 module.exports = repeatString;
