6a470794e8e1ec5584ae81de920db25d3b8290d1
Ash <hrksingh17@gmail.com>
2025-01-07 10:01:40 +0530

    sum all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (to, from) {
+  if (to < 0 || from < 0 || !Number.isInteger(to) || !Number.isInteger(from)) {
+    return "ERROR";
+  }
 
+  // swap values using array destructuring
+  if (to > from) [to, from] = [from, to];
+
+  //   if (to > from) {
+  //     let temp = from;
+  //     from = to;
+  //     to = temp;
+  //   }
+
+  let sum = 0;
+  for (let i = to; i <= from; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
